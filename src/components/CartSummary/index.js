51d2e75d99794f8ce6806b004e9f4e20b7d691f6
sadhana631 <indhusweety631@gmail.com>
2024-09-25 14:16:20 +0000

    nineth commit

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -16,14 +16,13 @@ const CartSummary = () => (
          <div className="cart-summary-container">
            <h1 className="order-total-value">
              <span className="order-total-label">Order Total:</span> Rs {total}
-             /-
            </h1>
            <p className="total-items">{cartList.length} Items in cart</p>
            <button type="button" className="checkout-button" d-sm-none">
              Checkout
            </button>
          </div>
-         <button type="button"
+         <button type="button">
            Checkout
          </button>
         </>
