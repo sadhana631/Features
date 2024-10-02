ea6b654a44a8269eebc2a66bef14074bd7cbd9d6
sadhana631 <indusweety631@gmail.com>
2024-10-02 07:06:13 +0000

    tenth commit

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -13,22 +13,23 @@ const CartSummary = () => (
 
       return (
         <>
-         <div className="cart-summary-container">
-           <h1 className="order-total-value">
-             <span className="order-total-label">Order Total:</span> Rs {total}
-           </h1>
-           <p className="total-items">{cartList.length} Items in cart</p>
-           <button type="button" className="checkout-button" d-sm-none">
+          <div className="cart-summary-container">
+            <h1 className="order-total-value">
+              <span className="order-total-label">Order Total:</span> Rs {total}
+              /-
+            </h1>
+            <p className="total-items">{cartList.length} Items in cart</p>
+            <button type="button" className="checkout-button d-sm-none">
              Checkout
            </button>
          </div>
-         <button type="button">
+         <button type="button" className="checkout-button d-lg-none">
            Checkout
          </button>
         </>
       )
     }}        
-  </CartContext>  
+  </CartContext.Consumer>  
 )
 
 export default CartSummary
